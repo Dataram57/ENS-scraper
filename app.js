fc19108f0d6258daffd03bfca84e7136f1432d6d
Dataram57 <dataram66657@gmail.com>
2023-08-25 19:57:41 +0200

    nahh

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+
+//assign config
+process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+});
+
+// START_BLOCK
+const startBlockNumber = 3;
+const endBlockNumber = 4;
